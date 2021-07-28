a8a2fa29d87ed012e0b2a5bfe0a18304c929e0a0
GroundZero211 <37128518+GroundZero211@users.noreply.github.com>
2021-07-28 13:22:14 +0800

    reedit file

--- friends.py
+++ friends.py
@@ -12,12 +12,16 @@ class Friends:
         response = requests.get('https://friends.roblox.com/v1/users/' + self.userid + '/friends?userSort=Alphabetical')
         json_data = json.loads(response.text)
         myfriends = json_data
+        key = list(myfriends.keys())
+        if key[0] == 'data' and myfriends[key[0]] == []:
+            return "Empty Friends"
+        elif key[0] == 'errors':
+            return "{} {}".format(myfriends[key[0]][0]['message'], myfriends[key[0]][0]['userFacingMessage'])
         list_ofriends = [myfriends['data'][i]['name'] + " => " + "is Online" if myfriends['data'][i]['isOnline'] else myfriends['data'][i]['name'] + " => " + "is Offline" for i in range(len(myfriends['data']))]
         return list_ofriends
 
 if __name__ == '__main__':
-    prompt = input("Enter your Roblox user-id: ")
+    prompt = input('Enter your user-id: ')
     user = Friends(prompt)
-    mfl = user.getfriendlist()
-    for i in mfl:
+    for i in user.getfriendlist():
         print(i)
