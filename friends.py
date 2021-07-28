bfa5ba3cc6b7005830e654294a90ab780377a800
GroundZero211 <37128518+GroundZero211@users.noreply.github.com>
2021-07-28 12:23:15 +0800

    Update friends.py

--- friends.py
+++ friends.py
@@ -12,7 +12,6 @@ class Friends:
         response = requests.get('https://friends.roblox.com/v1/users/' + self.userid + '/friends?userSort=Alphabetical')
         json_data = json.loads(response.text)
         myfriends = json_data
-        list_ofriends = []
         list_ofriends = [myfriends['data'][i]['name'] + " => " + "is Online" if myfriends['data'][i]['isOnline'] else myfriends['data'][i]['name'] + " => " + "is Offline" for i in range(len(myfriends['data']))]
         return list_ofriends
 
